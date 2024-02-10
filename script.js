22f06aa671d44230b0c24cd75dc96a93a368073b
Ajesh S <ajeshajesh9895@gmail.com>
2024-02-10 13:32:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const input = document.getElementById('inputBox')
+const buttons = Array.from(document.querySelectorAll('button'))
+
+let calculation = ''
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const btnValue = e.target.innerHTML
+
+    switch (btnValue) {
+      case '=':
+        try {
+          if (!calculation) return
+          calculation = new Function('return ' + calculation)()
+          if (Number(calculation) % 1 !== 0) {
+            calculation = Number(calculation).toFixed(3)
+          }
+          input.value = calculation
+        } catch {
+          input.value = 'Error'
+          calculation = ''
+        }
+        break
+      case 'AC':
+        calculation = ''
+        input.value = calculation
+        break
+      case 'DEL':
+        calculation = calculation.slice(0, -1)
+        input.value = calculation
+        break
+      default:
+        calculation += btnValue === 'X' ? '*' : btnValue
+        input.value = calculation
+    }
+  })
+})
